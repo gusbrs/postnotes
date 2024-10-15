9b4402781dc0111f2a33c3408f0d12a84ce53abb
gusbrs <19410606+gusbrs@users.noreply.github.com>
2024-10-15 08:36:42 -0300

    Drop unnecessary shebangs

--- build-3runs.lua
+++ build-3runs.lua
@@ -1,5 +1,3 @@
-#!/usr/bin/env texlua
-
 -- Some checks need more runs to build
 testfiledir  = "testfiles-3runs"
 checkruns    = 3
--- build-4runs.lua
+++ build-4runs.lua
@@ -1,5 +1,3 @@
-#!/usr/bin/env texlua
-
 -- Some checks need more runs to build
 testfiledir  = "testfiles-4runs"
 checkruns    = 4
